5a4eac7235088f418fb977179cceebdad3bd7365
Forla03 <fraforla2003@gmail.com>
2025-03-10 10:50:56 +0100

    Refactor translation utilities to check availability and list supported languages; update control panel settings to disabled

--- src/collective/translators/amazon/utility.py
+++ src/collective/translators/amazon/utility.py
@@ -16,16 +16,43 @@ class AmazonTranslatorFactory:
                             aws_secret_access_key=secret_key)
 
     def is_available(self):
-        return True
+        value = api.portal.get_registry_record("collective.translators.interfaces.IAmazonTranslateControlPanel.enabled")
+        return value
 
     def available_languages(self):
-        return []
+     try:
+        # Check if the translator client is available
+        if not self.translator:
+            return ["Not set"]
 
-    def translate_content(self, content, source_language, target_language):
-        content = content.lower()
-        res = self.translator.translate_text(
-            Text=content, SourceLanguageCode=source_language, TargetLanguageCode=target_language
-        )
-        return res["TranslatedText"]
+        # Get all available languages
+        response = self.translator.list_languages(DisplayLanguageCode='en')
+
+        # Extract source languages
+        source_lang_codes = [lang['LanguageCode'].lower() for lang in response['Languages']]
+
+        # Create a list of tuples (source_lang, target_lang)
+        translation_pairs = [(source_lang, target_lang) for source_lang in source_lang_codes for target_lang in source_lang_codes]
+       
+        return translation_pairs
+
+     except boto3.exceptions.Boto3Error:
+        return ["Not set"]
+
+    def translate_content(self, content, source_language, target_language): 
+        try:
+            res = self.translator.translate_text(
+                Text=content, SourceLanguageCode=source_language, TargetLanguageCode=target_language
+            )
+            return res["TranslatedText"]
+        except boto3.exceptions.Boto3Error as e:
+            # Retry with autodetect
+            try:
+                res = self.translator.translate_text(
+                    Text=content, TargetLanguageCode=target_language
+                )
+                return res["TranslatedText"]
+            except boto3.exceptions.Boto3Error:
+                return "Language not supported"
 
 AmazonTranslator = AmazonTranslatorFactory()
