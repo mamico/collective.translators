950f4666047afceb9d3418b7e0c333fb4c889bca
Francesco Forlani <114609133+Forla03@users.noreply.github.com>
2025-03-10 17:18:07 +0100

    Implement Amazon Translate integration and refactor translation utilities (#2)
    * Implement Amazon Transalte
    
    * Changed getUtility-IRegistry with plone.api and some minor fixes
    
    * Refactor translation utilities to check availability and list supported languages; update control panel settings to disabled
    
    ---------
    
    Co-authored-by: Mauro Amico <mauro.amico@gmail.com>

--- src/collective/translators/amazon/utility.py
+++ src/collective/translators/amazon/utility.py
@@ -1,6 +1,5 @@
 import boto3
-from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
+from plone import api
 from collective.translators.interfaces import IAmazonTranslateControlPanel
 
 class AmazonTranslatorFactory:
@@ -9,26 +8,51 @@ class AmazonTranslatorFactory:
 
     @property
     def translator(self):
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(IAmazonTranslateControlPanel, prefix="amazon_translate")
-        access_key = settings.access_key
-        secret_key = settings.secret_key
-        region_name = settings.region_name
+        access_key = api.portal.get_registry_record("collective.translators.interfaces.IAmazonTranslateControlPanel.access_key")
+        secret_key = api.portal.get_registry_record("collective.translators.interfaces.IAmazonTranslateControlPanel.secret_key")
+        region_name = api.portal.get_registry_record("collective.translators.interfaces.IAmazonTranslateControlPanel.region_name")
         return boto3.client("translate", region_name=region_name, 
                             aws_access_key_id=access_key, 
                             aws_secret_access_key=secret_key)
 
     def is_available(self):
-        return True
+        value = api.portal.get_registry_record("collective.translators.interfaces.IAmazonTranslateControlPanel.enabled")
+        return value
 
     def available_languages(self):
-        return []
+     try:
+        # Check if the translator client is available
+        if not self.translator:
+            return ["Not set"]
 
-    def translate_content(self, content, source_language, target_language):
-        content = content.lower()
-        res = self.translator.translate_text(
-            Text=content, SourceLanguageCode=source_language, TargetLanguageCode=target_language
-        )
-        return res["TranslatedText"]
+        # Get all available languages
+        response = self.translator.list_languages(DisplayLanguageCode='en')
+
+        # Extract source languages
+        source_lang_codes = [lang['LanguageCode'].lower() for lang in response['Languages']]
+
+        # Create a list of tuples (source_lang, target_lang)
+        translation_pairs = [(source_lang, target_lang) for source_lang in source_lang_codes for target_lang in source_lang_codes]
+       
+        return translation_pairs
+
+     except boto3.exceptions.Boto3Error:
+        return ["Not set"]
+
+    def translate_content(self, content, source_language, target_language): 
+        try:
+            res = self.translator.translate_text(
+                Text=content, SourceLanguageCode=source_language, TargetLanguageCode=target_language
+            )
+            return res["TranslatedText"]
+        except boto3.exceptions.Boto3Error as e:
+            # Retry with autodetect
+            try:
+                res = self.translator.translate_text(
+                    Text=content, TargetLanguageCode=target_language
+                )
+                return res["TranslatedText"]
+            except boto3.exceptions.Boto3Error:
+                return "Language not supported"
 
 AmazonTranslator = AmazonTranslatorFactory()
--- src/collective/translators/deepl/utility.py
+++ src/collective/translators/deepl/utility.py
@@ -1,7 +1,6 @@
 import deepl   
-from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
 from collective.translators.interfaces import IDeeplControlPanel
+from plone import api
 
 
 class DeeplTranslatorFactory:
@@ -16,26 +15,71 @@ class DeeplTranslatorFactory:
 
     @property
     def translator(self):
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(IDeeplControlPanel, prefix="deepl")
-        api_key = settings.api_key
+        api_key = api.portal.get_registry_record("collective.translators.interfaces.IDeeplControlPanel.api_key")
         return deepl.Translator(server_url=self.server_url, auth_key=api_key)
 
     def is_available(self):
-        return True
+        value =  api.portal.get_registry_record("collective.translators.interfaces.IDeeplControlPanel.enabled")
+        return value
 
     def available_languages(self):
-        # All
-        return []
+     try:
+        if not self.translator:
+            return {"Error": "Key not set"}
+
+        # Obtain the list of supported languages
+        source_languages = self.translator.get_source_languages()      
+        target_languages = self.translator.get_target_languages()
+
+        #Ensure that en e pt are included in the list of supported languages
+        additional_languages = ["en", "pt"]
+        for lang in additional_languages:
+            if lang not in [l.code.lower() for l in source_languages]:
+                source_languages.append(deepl.Language(code=lang, name=lang.upper()))
+            if lang not in [l.code.lower() for l in target_languages]:
+                target_languages.append(deepl.Language(code=lang, name=lang.upper()))
+        
+        # Create a list of supported translations
+        translation_pairs = [
+            (source_lang.code.lower(), target_lang.code.lower())
+            for source_lang in source_languages
+            for target_lang in target_languages
+            if source_lang.code != target_lang.code
+        ]
+
+        return translation_pairs
+
+     except deepl.DeepLException as e:
+          return {"Error": str(e)}
+
+
+    def normalize_language(self, language):
+        if language == "en":
+            return "en-us"
+        elif language == "pt":
+            return "pt-pt"
+        return language  
 
     def translate_content(self, content, source_language, target_language):
-        if self.autodetect_source_language:
-            res = self.translator.translate_text(content, target_lang=target_language)
-        else:
-            res = self.translator.translate_text(
-                content, source_lang=source_language, target_lang=target_language
-            )
-        return res.text
+        try:
+            target_language = self.normalize_language(target_language)
+            if self.autodetect_source_language:
+                res = self.translator.translate_text(content, target_lang=target_language)
+            else:
+                res = self.translator.translate_text(
+                    content, source_lang=source_language, target_lang=target_language
+                )
+            return res.text
+        except deepl.DeepLException as e:
+            # If the language is not supported, try to autodetect it
+            # If there's an other error, return an empty string
+            if not self.autodetect_source_language:
+                try:
+                    res = self.translator.translate_text(content, target_lang=target_language)
+                    return res.text
+                except deepl.DeepLException:
+                    return ""
+            return "Language not supported"
 
 
 DeeplTranslator = DeeplTranslatorFactory()
